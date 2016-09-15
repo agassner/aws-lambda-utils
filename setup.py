9683ee4f36926909e05a0da34f961bc72f85b2bd
Arthur Gassner <arthur.gassner@gmail.com>
2016-09-15 14:09:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aws-lambda-utils',
+    version='0.0.1',
+    description='AWS Lambda Function Python utilities',
+    long_description=long_description,
+    url='https://github.com/agassner/aws-lambda-utils',
+    author='Arthur Gassner',
+    author_email='@agassner',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='aws lambda function utilities',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['boto3'],
+    extras_require={
+    },
+    package_data={
+    },
+    data_files=[],
+    entry_points={
+    },
+)
